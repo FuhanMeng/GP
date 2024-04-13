cd1fdb2b676caa839d4385cf439a33ae770ffdd1
FuhanMeng <1722469764@qq.com>
2024-04-13 22:02:06 +0800

    4 13 3 commit

--- ultralytics-main/train.py
+++ ultralytics-main/train.py
@@ -6,17 +6,26 @@ if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/v8/yolov8n.yaml')
     model.load('yolov8n.pt') # loading pretrain weights
     model.train(data='dataset/data.yaml',
-                cache=False,
                 imgsz=640,
                 epochs=100,  #100
                 batch=128,  # 16
                 close_mosaic=10,  #10
+                patience=100,
+                cache=True,
                 workers=8,  # 8
-                device='0',
-                optimizer='SGD', # using SGD
-                # resume='', # last.pt path
+                device=0,
+                optimizer='SGD', # using SGD\Adam, AdamW, NAdam, RAdam, RMSProp
+                save=True,
+                save_period=5,
+                resume=True, # last.pt path
+                lr_0=0.01, # SGD=1E-2，Adam=1E-3
+                # cos_lr, #
                 # amp=False, # close amp
                 # fraction=0.2,
+                # profile, # ONNX
+                # momentum\weight_decay\warmup.....
+                val=True,
+                plots=True,
                 project='runs/train',
                 name='exp',
                 )
