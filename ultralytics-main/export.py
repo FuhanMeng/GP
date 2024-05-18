cd6c52d165f4150fb6835ac7f5bff4c5bb9456ab
FuhanMeng <1722469764@qq.com>
2024-05-18 14:00:34 +0800

    5181commit

--- ultralytics-main/export.py
+++ ultralytics-main/export.py
@@ -5,5 +5,6 @@ from ultralytics import YOLO
 # onnx onnxsim onnxruntime onnxruntime-gpu
 
 if __name__ == '__main__':
-    model = YOLO('../deploy/yolov8n-distill-cwd-0506-exp/weights/best.pt')  # 直接指定权重文件，不要yaml
-    model.export(format='onnx', simplify=True, opset=13)
+    model = YOLO('./deploy/4yolov8n-pconvhead-distill-cwd-L2-0506-exp/weights/best.pt')  # 直接指定权重文件，不要yaml
+    # model.export(format='onnx', simplify=True, opset=13)
+    model.export(format='torchscript')
